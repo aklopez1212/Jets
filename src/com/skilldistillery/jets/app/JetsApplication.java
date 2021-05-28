9547a9d588cee2c19598c2b01204b4eaf3ec8972
Alex Lopez <alexander.k.lopez@gmail.com>
2021-05-28 15:23:24 -0600

    initial commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entity.AirField;
+
+public class JetsApplication {
+private AirField airField;
+	public static void main(String[] args) {
+		
+
+	}
+
+}
